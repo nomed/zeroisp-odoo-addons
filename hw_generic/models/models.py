d2793016651e2c1c7a23ab73c2353e4f5b750536
nomed <daniele@zeroisp.com>
2017-09-03 00:47:48 +0200

    test model and views

--- hw_generic/models/models.py
+++ hw_generic/models/models.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import logging
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+
+_logger = logging.getLogger(__name__)
+
 
 # class hw_generic(models.Model):
 #     _name = 'hw_generic.hw_generic'
@@ -13,3 +17,10 @@ from odoo import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+class HwGenericConfiguration(models.Model):
+    _name = 'hw_generic.configuration'
+
+    name = fields.Char(required=True, help='Name of this Mercury configuration')
+    merchant_id = fields.Char(string='Merchant ID', required=True, help='ID of the merchant to authenticate him on the payment provider server')
+    merchant_pwd = fields.Char(string='Merchant Password', required=True, help='Password of the merchant to authenticate him on the payment provider server')
